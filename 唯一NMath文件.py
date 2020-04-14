51b460ca17e54ac14e0d02fc0cabbab15b9a67e4
xnv9 <63526533+xnv9@users.noreply.github.com>
2020-04-14 14:13:16 +0800

    Update 唯一NMath文件.py

--- 唯一NMath文件.py
+++ 唯一NMath文件.py
@@ -5,6 +5,22 @@
 #质数查询
 def prime_check(x):
     try:
-		    x%2=fn
-		except Exception:
-		    return "Error"
+        x%2=fn
+        if fn==0:
+            return False
+        elif fn==1:
+            (x+1)/2=sn
+        else:
+            return "Error"
+        i=2
+        while i<sn:
+            x//sn=test
+            if test==True:
+                return False
+            elif test==False:
+                i+=1
+            else:
+                return "Error"
+        return True
+    except Exception:
+        return "Error"
